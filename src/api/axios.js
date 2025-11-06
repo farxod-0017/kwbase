1b7918a6f1f886fa236a1213f9a830eebdff2f38
farxod_0017 <farxodegamnazarov2@gmail.com>
2025-11-06 13:55:16 +0500

    change API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dev.usderp.uz/baza", // backend API manzilingiz
+  baseURL: "https://api.dev.usderp.uz/crm", // backend API manzilingiz
   headers: {
     "Content-Type": "application/json",
   },
